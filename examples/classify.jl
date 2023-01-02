b0f70ab52b8fdad6c51c38fa614457179eee72c7
Tomáš Mlynář <tom.mlynar1@gmail.com>
2023-01-02 15:35:48 +0100

    example updated

--- examples/classify.jl
+++ examples/classify.jl
@@ -1,9 +1,24 @@
 using Revise
 using Projekt_mlynatom
 using DataFrames
+using StatsPlots
 
 df = read_csv_to_df("data/train.csv")
 
+### View data
+#histogram of siblings
+@df df histogram(:SibSp, title="Siblings/Spouses")
+
+#histogram of Passenger classes
+@df df histogram(:Pclass, title="Passenger classes")
+
+@df df histogram(:Age, title="Age", legend=false)
+
+@df df histogram(:Parch, title="Parch")
+
+@df df histogram(:Fare)
+
+### Prepare data
 one_hot_sex = categorical_to_one_hot(df.Sex)
 
 X = Matrix(df[:, [:Pclass, :SibSp, :Parch]])
@@ -11,6 +26,7 @@ X = hcat(X, one_hot_sex)
 
 y = df.Survived
 
+### Logistic regression
 #X = standardize(X)
 
 w = log_reg(X, y)
@@ -19,7 +35,7 @@ preds = predict(X, w)
 
 error = compute_class_error(y, preds)
 
-### on test
+## on test
 
 df_test = read_csv_to_df("data/test.csv")
 categorical_sex = df_test.Sex
@@ -33,3 +49,13 @@ preds = predict(X, w)
 new_df = DataFrame(PassengerId=df_test.PassengerId, Survived=Int8.(preds))
 
 save_df_to_csv(new_df, "data/my_submission.csv")
+
+### NN
+X_train, y_train, X_dev, y_dev = split_dataset(X, y)
+
+#standardize
+X_train_s, X_dev_s = standardize(X_train, X_dev)
+
+my_network = Chain(
+    Dense(size())
+)
--- src/data_preparation.jl
+++ src/data_preparation.jl
@@ -1,8 +1,9 @@
 using CSV
 using DataFrames
 using Statistics
+using Random
 
-export categorical_to_one_hot, standardize
+export categorical_to_one_hot, standardize, split_dataset
 
 function categorical_to_one_hot(vec::AbstractVector{<:AbstractString})
     unique_vals = unique(vec)
@@ -15,8 +16,34 @@ function categorical_to_one_hot(vec::AbstractVector{<:AbstractString})
     return ret_mat
 end
 
-function standardize(X::Matrix{<:Number})
-    col_mean = mean(X; dims=1)
-    col_std = std(X; dims=1)
+function standardize(X::Matrix{<:Number}; dims=1)
+    col_mean = mean(X; dims=dims)
+    col_std = std(X; dims=dims)
     return (X .- col_mean) ./ col_std
+end
+
+function standardize(X_train::Matrix{<:Number}, X_dev::Matrix{<:Number}; dims=1)
+    col_mean = mean(X_train; dims=dims)
+    col_std = std(X_train; dims=dims)
+
+    X_train_s = (X_train .- col_mean) ./ col_std
+    X_dev_s = (X_train .- col_mean) ./ col_std
+
+    return X_train_s, X_dev_s
+end
+
+function split_dataset(X, y; dev_ratio = 0.1)
+    n = length(y)
+    n_dev = round(Int64, dev_ratio*n)
+
+    idx_rand = randperm(n)
+    idx_dev = idx_rand[1:n_dev] 
+    idx_train = idx_rand[n_dev+1:end]
+
+    X_train = X[idx_train, :] 
+    X_dev = X[idx_dev, :]
+    y_train = y[idx_train]
+    y_dev = y[idx_dev]
+
+    return X_train, y_train, X_dev, y_dev
 end
